0e7c0080d4ec557275ea2b1b000d1577eb0b8d1b
민현기 <bluesky0990@naver.com>
2020-07-07 13:41:24 +0900

    Modify Main.java, Main.Activity

--- app/src/main/java/com/nellem/cloud1/MainActivity.java
+++ app/src/main/java/com/nellem/cloud1/MainActivity.java
@@ -19,6 +19,9 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
     FirebaseDatabase db = FirebaseDatabase.getInstance ();
@@ -53,18 +56,15 @@ public class MainActivity extends AppCompatActivity {
         myRef.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-                String data = snapshot.getValue().toString();
-                //textView.append(data.toString() + "\n");
-                data = data.replaceAll("\\{", "");
-                data = data.replaceAll("\\}", "");
-                data = data.replaceAll("date=", "");
-                data = data.replaceAll("temperature=", "");
-                data = data.replaceAll("humidity=", "");
-                String[] sResult = data.split(",");
+                Map<String, Object> dataDict = new HashMap<>();
+                dataDict = (Map<String, Object>) snapshot.getValue();
+                String hu = dataDict.get("humidity").toString();
+                String te = dataDict.get("temperature").toString();
+                String ti = dataDict.get("date").toString();
 
-                textView.setText(sResult[0]);
-                textView2.setText(sResult[1] + "℃");
-                textView3.setText(sResult[2] + "%");
+                textView.setText(ti);
+                textView2.setText(te + "℃");
+                textView3.setText(hu + "%");
             }
 
             @Override
